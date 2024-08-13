8e5859afa3b5b2dd2ecb0407972388e8ed1cf997
horizonteMedic <sistemas@horizontemedic.com>
2024-08-12 23:12:28 -0500

    cambios

--- proyMina/src/proyMina/vista/interfaces/FichaTriaje.java
+++ proyMina/src/proyMina/vista/interfaces/FichaTriaje.java
@@ -12,8 +12,10 @@ import java.awt.Component;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
+import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -30,6 +32,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
@@ -44,6 +47,7 @@ import org.jdesktop.swingx.autocomplete.AutoCompleteDecorator;
 import org.json.JSONObject;
 import proyMina.modelo.DisableSSLVerification;
 import proyMina.modelo.clsGlobales;
+import sun.misc.BASE64Decoder;
 public final class FichaTriaje extends javax.swing.JInternalFrame {
  clsConnection oConn = new clsConnection();
    clsFunciones  oFunc = new clsFunciones();
@@ -1294,9 +1298,24 @@ boolean bResultado=true;
                 consumirApiSello();
                 Map parameters = new HashMap(); 
                 parameters.put("Norden",cod);          
+              //  InputStream targetStream = IOUtils.toInputStream(base64String);  
+              //
+                BufferedImage image = null;
+                byte[] imageByte;
 
-                InputStream targetStream = new ByteArrayInputStream(base64String.getBytes());     
-                parameters.put("Firma",targetStream);             
+                BASE64Decoder decoder = new BASE64Decoder();
+                    imageByte = decoder.decodeBuffer(base64String);
+                ByteArrayInputStream bis = new ByteArrayInputStream(imageByte);
+                image = ImageIO.read(bis);
+                bis.close();
+                
+                
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                ImageIO.write(image, "png", baos); 
+                InputStream stream = new ByteArrayInputStream(baos.toByteArray());
+                
+                
+                parameters.put("Firma",stream);             
 
                 System.out.println("los parametros son: "+parameters);
                   try 
